a2470a8147b8ec0a59aafdf6f6fe6d6528701831
MidNightCodder47 <77gta51@gmail.com>
2025-06-15 00:21:12 +0300

    Creating test + passing it

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import classes
 
-# создание 1 вопроса с вводом задания и нужного ответа
-quest1 = classes.exercise(question_text=input("Enter ur question \n"), 
-                          question_answer= input("Enter correct answer\n"))
 
-print(quest1.question_text) # Показ вопроса
-print(quest1.correctness(input("Ur answer \n"))) # проверка вводимого ответа заданному
+test1 = classes.test(input("Enter test title \n"),
+                     int(input("Enter amount of exercises \n")))
+test1.test_add_exr()
+test1.test_start()
