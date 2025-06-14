182496a51fd6f005e60bbbdd2d7f4f96f6e5c4c5
MidNightCodder47 <77gta51@gmail.com>
2025-06-14 23:09:22 +0300

    Creating task + call

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-# start
+import classes
+
+# создание 1 вопроса с вводом задания и нужного ответа
+quest1 = classes.exercise(question_text=input("Enter ur question \n"), 
+                          question_answer= input("Enter correct answer\n"))
+
+print(quest1.question_text) # Показ вопроса
+print(quest1.correctness(input("Ur answer \n"))) # проверка вводимого ответа заданному
